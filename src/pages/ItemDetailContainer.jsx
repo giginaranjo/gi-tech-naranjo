5d4a9fa7cd90c8df650ec36caacef6fc766817c5
giginaranjo <gilarynaranjop@gmail.com>
2023-12-21 01:52:41 -0300

    diseño detalle producto

--- src/pages/ItemDetailContainer.jsx
+++ src/pages/ItemDetailContainer.jsx
@@ -1,5 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./ItemDetailContainer.css"
+
 import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 import { useGetItemProduct } from '../hooks/useProducts';
 import { useParams } from 'react-router-dom';
@@ -10,26 +13,36 @@ const ItemDetailContainer = () => {
   const { productData } = useGetItemProduct(id);
 
   return (
-
-    <Card key={productData.id} className='tarjeta' style={{ width: '20rem', border: "1px solid #79fd39" }}>
-      <Card.Img variant="top" src={productData.thumbnail} />
-      <Card.Body>
-        <Card.Title>{productData.title}</Card.Title>
-        <Card.Text>
-          {productData.description}
-        </Card.Text>
-      </Card.Body>
-    </Card>
+    <div className='tarjetita'>
+      <Card className='card-items' key={productData.id}>
+        <div className='img-products'>
+          {productData.images ? productData.images.map((image) => {
+            return (
+              <Card.Img className='imagen' variant="top" src={image} />
+            )
+          }) : null}
+        </div>
+        <Card.Body className='info-principal'>
+          <Card.Title>{productData.title}</Card.Title>
+          <Card.Text>
+            {productData.description}
+          </Card.Text>
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+          <ListGroup.Item>Category: {productData.category}</ListGroup.Item>
+          <ListGroup.Item>Brand: {productData.brand}</ListGroup.Item>
+          <ListGroup.Item>Price: ${productData.price}</ListGroup.Item>
+        </ListGroup>
+        <Card.Body className='secc-btn'>
+          <Card.Link className='btn' href="#">Add to cart</Card.Link>
+        </Card.Body>
+      </Card>
+    </div>
   )
 }
 
 export default ItemDetailContainer
 
 
-/* {productData.images ? productData.images.map((image) => {
-          return (
-            <Carousel.Item>
-              <ExampleCarouselImage src={image} />
-            </Carousel.Item>
-          )
-        }) : null} */
+
+
